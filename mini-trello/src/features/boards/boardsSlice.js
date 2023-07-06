d8e4f2a5f11147ac0515d64d917296b08ade9b53
Gautham <gauthamd.das@gmail.com>
2023-07-06 22:31:39 +0530

    added boardSlice with board operations

---
+++ mini-trello/src/features/boards/boardsSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const boardsSlice = createSlice({
+  name: 'boards',
+  initialState: [],
+  reducers: {
+    createBoard: (state, action) => {
+      state.push({
+        id: action.payload,
+        name: `Board ${state.length + 1}`,
+        stages: [],
+      });
+    },
+    createStage: (state, action) => {
+      const board = state.find(board => board.id === action.payload.boardId);
+      if (board) {
+        board.stages.push({
+          id: action.payload.stageId,
+          name: `Stage ${board.stages.length + 1}`,
+          tasks: [],
+        });
+      }
+    },
+    createTask: (state, action) => {
+      const { boardId, stageId, taskId, content } = action.payload;
+      const board = state.find(board => board.id === boardId);
+      if (board) {
+        const stage = board.stages.find(stage => stage.id === stageId);
+        if (stage) {
+          stage.tasks.push({ id: taskId, content });
+        }
+      }
+    },
+    moveTask: (state, action) => {
+      const { draggableId, source, destination } = action.payload;
+      console.log(action.payload)
+      const sourceStage = state
+        .flatMap(board => board.stages)
+        .find(stage => stage.id === source.droppableId);
+      const destinationStage = state
+        .flatMap(board => board.stages)
+        .find(stage => stage.id === destination.droppableId);
+
+      const task = sourceStage.tasks.find(task => task.id === draggableId);
+      if (task) {
+        sourceStage.tasks.splice(source.index, 1);
+        destinationStage.tasks.splice(destination.index, 0, task);
+      }
+    },
+  },
+});
+
+export const { createBoard, createStage, createTask, moveTask } = boardsSlice.actions;
+export default boardsSlice.reducer;
